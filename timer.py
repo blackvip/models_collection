5d98e60915a632cc7eb466ee271001e2c966a21b
blackvip <hhr2wd@163.com>
2018-09-04 22:42:30 +0800

    add multi_layer_timer

---
+++ timer.py
@@ -0,0 +1,20 @@
+import time
+import psutil
+from contextlib import contextmanager
+
+class multi_layer_timer:
+    def __init__(self):
+        self.timer_depth = -1
+
+    @contextmanager
+    def timer(name):
+        t0 = time.time()
+        self.timer_depth += 1
+        yield
+        pid = os.getpid()
+        py = psutil.Process(pid)
+        memoryUse = py.memory_info()[0] / 2. ** 30
+        print('----'*self.timer_depth + f'>>[{name}] done in {time.time() - t0:.0f} s ---> memory used: {memoryUse:.4f} GB', '')
+        if(self.timer_depth == 0):
+            print('\n')
+        self.timer_depth -= 1
