7ceb9ea2beaeef3b6140ff8b89a98ea193dd5fa8
whoamI <hhr358@mail.ustc.edu.cn>
2018-09-09 20:56:38 +0800

    update iou metric

--- iou.py
+++ iou.py
@@ -1,56 +1,23 @@
 import numpy as np
 
-def iou_metric(y_true_in, y_pred_in):
-    labels = y_true_in
-    y_pred = y_pred_in
+iou_thresholds = np.array([0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
+
+def iou(img_true, img_pred):
+    i = np.sum((img_true*img_pred) >0)
+    u = np.sum((img_true + img_pred) >0)
+    if u == 0:
+        return u
+    return i/u
+
+def iou_metric_batch(imgs_true, imgs_pred):
+    num_images = len(imgs_true)
+    scores = np.zeros(num_images)
     
-    true_objects = 2
-    pred_objects = 2
-
-    intersection = np.histogram2d(labels.flatten(), y_pred.flatten(), bins=(true_objects, pred_objects))[0]
-
-    # Compute areas (needed for finding the union between all objects)
-    area_true = np.histogram(labels, bins = true_objects)[0]
-    area_pred = np.histogram(y_pred, bins = pred_objects)[0]
-    area_true = np.expand_dims(area_true, -1)
-    area_pred = np.expand_dims(area_pred, 0)
-
-    # Compute union
-    union = area_true + area_pred - intersection
-
-    # Exclude background from the analysis
-    intersection = intersection[1:,1:]
-    union = union[1:,1:]
-    union[union == 0] = 1e-9
-
-    # Compute the intersection over union
-    iou = intersection / union
-
-    # Precision helper function
-    def precision_at(threshold, iou):
-        matches = iou > threshold
-        true_positives = np.sum(matches, axis=1) == 1   # Correct objects
-        false_positives = np.sum(matches, axis=0) == 0  # Missed objects
-        false_negatives = np.sum(matches, axis=1) == 0  # Extra objects
-        tp, fp, fn = np.sum(true_positives), np.sum(false_positives), np.sum(false_negatives)
-        return tp, fp, fn
-
-    # Loop over IoU thresholds
-    prec = []
-    for t in np.arange(0.5, 1.0, 0.05):
-        tp, fp, fn = precision_at(t, iou)
-        if (tp + fp + fn) > 0:
-            p = tp / (tp + fp + fn)
+    for i in range(num_images):
+        if imgs_true[i].sum() == imgs_pred[i].sum() == 0:
+            scores[i] = 1
         else:
-            p = 0
-        prec.append(p)
-    
-    return np.mean(prec)
+            scores[i] = (iou_thresholds <= iou(imgs_true[i], imgs_pred[i])).mean()
+            
+    return scores.mean()
 
-def iou_metric_batch(y_true_in, y_pred_in):
-    batch_size = y_true_in.shape[0]
-    metric = []
-    for batch in range(batch_size):
-        value = iou_metric(y_true_in[batch], y_pred_in[batch])
-        metric.append(value)
-    return np.mean(metric)
