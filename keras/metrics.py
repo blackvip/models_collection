91f9df82073568a20d3d6ea06f0a703768243dae
whoamI <hhr358@mail.ustc.edu.cn>
2018-09-11 20:49:35 +0800

    udate keras iou metric

--- keras/metrics.py
+++ keras/metrics.py
@@ -40,28 +40,42 @@ def faster_iou_metric(label, pred):
     return metric_value
 
 
-iou_thresholds = np.array([0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95])
 def iou(img_true, img_pred):
     i = np.sum((img_true*img_pred) >0)
     u = np.sum((img_true + img_pred) >0)
     if u == 0:
         return u
     return i/u
-def threshold_mean_iou(imgs_true, imgs_pred):
-    num_images = len(imgs_true)
-    scores = np.zeros(num_images)
-    
-    for i in range(num_images):
-        if imgs_true[i].sum() == imgs_pred[i].sum() == 0:
-            scores[i] = 1
-        else:
-            # scores[i] = (iou_thresholds <= iou(imgs_true[i], imgs_pred[i])).mean()
-            scores[i] = np.mean([iou(imgs_true[i], (threshold<=imgs_pred[i]).astype('uint8')) for threshold in iou_thresholds])           
-    return scores.mean()
-def threshold_mean_iou_metric(label, pred):
-    metric_value = tf.py_func(threshold_mean_iou, [label, pred], tf.float32)
-    return metric_value
 
+def compute_ious(imgs_true, imgs_pred):
+    num_images = len(imgs_true)
+    num_preds  = len(imgs_pred)
+    scores = np.zeros((num_images, num_preds))
+    for i in range(num_images):
+        for j in range(num_preds):
+            if imgs_true[i].sum() == imgs_pred[j].sum() == 0:
+                scores[i, j] = 1
+            else:
+                scores[i, j] = iou(imgs_true[i], imgs_pred[j])
+    return scores
+
+def compute_precision_at(ious, threshold):
+    mx1 = np.max(ious, axis=0)
+    mx2 = np.max(ious, axis=1)
+    tp = np.sum(mx2 >= threshold)
+    fp = np.sum(mx2 < threshold)
+    fn = np.sum(mx1 < threshold)
+    return float(tp) / (tp + fp + fn)
+
+def compute_eval_metric(gt, predictions):
+    thresholds = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]
+    ious = compute_ious(gt, predictions)
+    precisions = [compute_precision_at(ious, th) for th in thresholds]
+    return sum(precisions) / len(precisions)
+
+def threshold_mean_iou_metric(gt, predictions):
+    metric_value = tf.py_func(compute_eval_metric, [gt, predictions], tf.float32)
+    return metric_value
 
 def mean_iou(y_true, y_pred):
     y_pred = tf.round(y_pred)
